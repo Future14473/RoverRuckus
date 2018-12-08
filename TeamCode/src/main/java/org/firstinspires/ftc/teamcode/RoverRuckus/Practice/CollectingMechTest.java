8a7a027d16b733455ffe45b324cc550ab82711b7
Benjamin Ye <BobBenBan@users.noreply.github.com>
2018-12-07 20:21:17 -0800

    PLZ READ COMMIT DESCRIPTION THANKS
    SO I started from scratch with the ftc-app thing so all the scripts and what not will run and things aren't red everywhere, then just copied all the Teamcode java files and now it works... since we aren't using any external libraries/code why don't we make this branch the actual branch and start from here
    
    Also
    added ./doc to .gitignore

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/Practice/CollectingMechTest.java
@@ -0,0 +1,48 @@
+/*
+package org.firstinspires.ftc.teamcode.RoverRuckus.Practice;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp(name = "CollectingMechTest", group = "Test")
+public class CollectingMechTest extends OpMode {
+
+    HardwareTestBot robot = new HardwareTestBot();
+    HardwareMap hwMap;
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.a){
+            robot.Succ.setPower(0.4);
+        } else if(gamepad1.b) {
+            robot.Succ.setPower(-0.4);
+        } else {
+            robot.Succ.setPower(0);
+        }
+
+        if(gamepad1.dpad_up){
+            robot.Pivot.setPower(0.7);
+        } else if(gamepad1.dpad_down){
+            robot.Pivot.setPower(-0.7);
+        } else {
+            robot.Pivot.setPower(0);
+        }
+
+        if(gamepad1.right_bumper) {
+            robot.Pivot2.setPower(0.7);
+            //robot.Pivot3.setPower(0.7);
+        } else if(gamepad1.left_bumper) {
+            robot.Pivot2.setPower(-0.7);
+            //robot.Pivot3.setPower(-0.7);
+        } else {
+            robot.Pivot2.setPower(0);
+            //robot.Pivot3.setPower(0);
+        }
+    }
+}*/
