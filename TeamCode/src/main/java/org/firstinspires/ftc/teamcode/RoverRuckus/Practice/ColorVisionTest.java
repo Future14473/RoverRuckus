8936a2166133cea80242bce9d8eee7a87eb55460
Benjamin Ye <BobBenBan@users.noreply.github.com>
2018-12-07 20:21:17 -0800

    PLZ READ COMMIT DESCRIPTION THANKS
    SO I started from scratch with the ftc-app thing so all the scripts and what not will run and things aren't red everywhere, then just copied all the Teamcode java files and now it works... since we aren't using any external libraries/code why don't we make this branch the actual branch and start from here
    
    Also
    added ./doc to .gitignore

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/Practice/ColorVisionTest.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.RoverRuckus.Practice;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "ColorVisionTest", group = "Test")
+public class ColorVisionTest extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
