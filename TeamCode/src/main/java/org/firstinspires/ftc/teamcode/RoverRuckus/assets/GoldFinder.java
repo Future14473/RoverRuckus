b38091832ace16f01a8d39eb72aecf346f42663b
Ben B <benbielin02@gmail.com>
2018-12-22 19:08:28 -0800

    Added a no steal key

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/assets/GoldFinder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/assets/GoldFinder.java
@@ -67,7 +67,7 @@ public class GoldFinder {
 	 * Once you've obtained a license key, copy the string from the Vuforia web site
 	 * and paste it in to your code on the next line, between the double quotes.
 	 */
-	private static final String VUFORIA_KEY = " -- YOUR NEW VUFORIA KEY GOES HERE  --- ";
+	private static final String VUFORIA_KEY = "Aavay7//////AAABmS26wV70nE/XoqC91tMM/rlwbqInv/YUads4QRll085q/yT+qW0qdyrUwXPXbvwDkGhnffFMGIizzvfrXviNCbfAAgJzSwDJuL0MJl3LRE2FU4JMKKU2v7V+XGChhH91BXriKEtx4PDCq5DwSpCT1TP3XSJrouflaIEdqxTcUz/LaIEh4phJs35awBUu+g+4i3EKMJBsYWyJ0V9jdI5DLCVhXkKtBpKgJbO3XFx40Ig/HFXES1iUaOk2fj9SG/jRUsWLH1cs35/g289Xs6BTQTHnGpX9bcOvK0m4NkhogjqbT7S76O91jeheUZwazesROu848shb317YhWIclBSR/vV9/I2fT+485YdwnaxuS8K9";
 	
 	/**
 	 * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
