4aae299db6ed6d629df1469c63370b2d1d5d4f9b
Ben B <benbielin02@gmail.com>
2018-12-28 20:25:48 -0800

    asdf
    asdf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/testing/AutoTestNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/testing/AutoTestNew.java
@@ -26,16 +26,12 @@ public class AutoTestNew extends LinearOpMode {
 		goldLooker.start();
 		robot.drive.move(270, 0.5, 0.07);
 		robot.drive.move(0, 0.5, 0.05);
-		robot.drive.move(55, 1f, .57f);
-		robot.drive.waitForDone();
 		
 		knockOffGold();
+		PutMarkerInDepot();
+		ParkInCrater();
 	}
 	public boolean runTo(int encoder, DcMotor motor) {
-		int dir;
-		
-		if (motor.getCurrentPosition() >= encoder) dir = 1;
-		if (motor.getCurrentPosition() < encoder) dir = -1;
 		motor.setPower(1);
 		//decreasing
 		while (Math.abs(encoder - motor.getCurrentPosition()) > 100) {
@@ -47,10 +43,13 @@ public class AutoTestNew extends LinearOpMode {
 	
 	private void knockOffGold() {
 		boolean found = false;
+		robot.drive.move(55, 1, .75); //move forwards
+		
 		int i;
 		int look = -1; // -1 means nothing, 0 means white, 1 means gold
 		for (i = 1; i >= -1; i--) {// -1 is left, 0 is center, 1 is right position
-			if (look == -1) {
+			
+			while (look == -1) {
 				look = goldLooker.look();
 				while (look == -1) {
 					look = closerLook();
@@ -64,7 +63,7 @@ public class AutoTestNew extends LinearOpMode {
 			}
 			
 			if (i != -1) {// has not traverse the 3 positions yet
-				robot.drive.move(270, 1f, 17f / 36);
+				robot.drive.move(278, 1f, 19f / 36);
 				look = -1;
 			}
 		}
@@ -76,10 +75,20 @@ public class AutoTestNew extends LinearOpMode {
 		//robot.drive.move(90, 1f, (1 - i) * 17f / 36);
 		
 		if (i == 1) {
-			robot.drive.move(270, 1f, 34f / 36);
+			robot.drive.move(278, 1f, 38f / 36);
 		} else if (i == 0) {
-			robot.drive.move(270, 1f, 17f / 36);
+			robot.drive.move(278, 1f, 19f / 36);
 		}
+		
+		//robot.drive.move(180, 1f, 0.4f);
+		//robot.drive.stopRobot();
+
+            /*
+            if (found) {
+                break;
+            }
+            */
+		//}
 		
 	}
 	
@@ -102,5 +111,24 @@ public class AutoTestNew extends LinearOpMode {
 		robot.drive.move(0, 0.3f, 2f / 36);
 		look = goldLooker.look();
 		return look;
+	}
+	private void PutMarkerInDepot() {
+		robot.drive.move(260, 0.5, 1);
+		robot.drive.turn(90, 0.5);
+		robot.drive.move(180, 0.5, 1);
+		robot.Marker.setPosition(0.84);
+		sleep(2000);
+		robot.Flicker.setPosition(0.65);
+		sleep(5000);
+	}
+	
+	private void ParkInCrater() {
+		robot.drive.move(0, 0.5, 2.5);
+		robot.Arm.setPower(-1);
+		sleep(1000);
+		robot.Arm.setPower(0);
+		robot.Rotation.setPower(1);
+		sleep(1000);
+		robot.Rotation.setPower(0);
 	}
 }
