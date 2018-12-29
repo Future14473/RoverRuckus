bf905555f06083d294d25199ca487cf2503f7883
Benjamin Ye <BobBenBan@users.noreply.github.com>
2018-12-29 12:43:28 -0800

    Revert "Renamed packages"
    This reverts commit e899d38a52bcf8b69510f66c02a35cf03c5dabf7.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/Practice/MecanumWheelsTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/Practice/MecanumWheelsTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.RoverRuckus.teleOps;
+package org.firstinspires.ftc.teamcode.RoverRuckus.Practice;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/testing/GoldTripleLook.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/testing/GoldTripleLook.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.RoverRuckus.autonomous;
+package org.firstinspires.ftc.teamcode.RoverRuckus.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
