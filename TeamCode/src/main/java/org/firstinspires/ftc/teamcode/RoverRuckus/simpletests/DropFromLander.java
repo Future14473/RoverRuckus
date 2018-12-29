bbf8ab33f7572cb3f99f05d4066541c0fe1bb72a
Ben B <benbielin02@gmail.com>
2018-12-28 17:04:57 -0800

    Merged other repo manually

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/simpletests/DropFromLander.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/simpletests/DropFromLander.java
@@ -7,32 +7,40 @@ import org.firstinspires.ftc.teamcode.RoverRuckus.util.Robot;
 
 @Autonomous(name = "DropFromLander", group = "Test")
 public class DropFromLander extends LinearOpMode {
-	private Robot robot = new Robot();
-	
-	@Override
-	public void runOpMode() throws InterruptedException {
-		waitForStart();
-		robot.init(hardwareMap);
-		robot.Hooke.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-		robot.Hooke.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-		
-		runto(robot.Hooke.getCurrentPosition() - 31000, robot.Hooke);
-	}
-	
-	public static boolean runto(int encoder, DcMotor motor) {
-		int dir = 0;
-		
-		if (motor.getCurrentPosition() > encoder) dir = 1;
-		if (motor.getCurrentPosition() < encoder) dir = -1;
-		//dir=1;
-		motor.setPower(dir);
-		//decreasing
-		while (!((-1 * dir * (motor.getCurrentPosition() - encoder)) > 100)) {
-			//wait
-		}
-		
-		motor.setPower(0);
-		
-		return true;
-	}
+    Robot robot = new Robot();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        robot.init(hardwareMap);
+        robot.Hooke.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.Hooke.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        runTo(robot.Hooke.getCurrentPosition()-31000,robot.Hooke);
+
+        robot.drive.move(270, 0.5, 0.1);
+        robot.drive.move(0, 0.5, 0.1);
+        robot.drive.move(90, 0.5, 0.1);
+
+    }
+    public boolean runTo(int encoder, DcMotor motor){
+        int dir=0;
+
+        if(motor.getCurrentPosition()>encoder)dir=1;
+        if(motor.getCurrentPosition()<encoder)dir=-1;
+        //dir=1;
+        motor.setPower(dir);
+        //decreasing
+        while(!((-1*dir*(motor.getCurrentPosition()-encoder))>100)){
+            //wait
+
+        }
+        waitForDone(robot.Hooke);
+        motor.setPower(0);
+
+        return true;
+    }
+    public void waitForDone(DcMotor motor) {
+        while (motor.isBusy()) ;
+    }
 }
