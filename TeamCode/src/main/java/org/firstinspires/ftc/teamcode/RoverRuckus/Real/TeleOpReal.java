2647a3de9844bb4feef5a7694aeb902dc39e83a3
Benjamin Ye <BobBenBan@users.noreply.github.com>
2019-01-02 15:52:10 -0800

    Updates to Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/Real/TeleOpReal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/Real/TeleOpReal.java
@@ -14,22 +14,26 @@ public class TeleOpReal extends OpMode {
 		robot.init(hardwareMap);
 	}
 	
+	private boolean pastGamepad1X = false;
+	
 	@Override
 	public void loop() {
+		//GAMEPAD 1
+		//MOVE BOT
+		double speedMult = gamepad1.right_bumper ? 10 : (gamepad1.left_bumper ? 0.4 : 1);
 		double angle = Math.atan2(gamepad1.left_stick_x, -gamepad1.left_stick_y);
-		double speed = Math.pow(Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y), 1.7) * 1.4f;
+		double speed = Math.pow(Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y), 1.7) * speedMult;
 		double turnRate = gamepad1.right_stick_x * 2; //prioritize turning over moving.
-		//telemetry.addData("Stick angle:",angle);
 		robot.drive.moveAt(angle, speed, turnRate);
-		
-		if (gamepad1.left_bumper) {
+		//TAPE
+		if (gamepad1.a) {
 			robot.tape.setPower(-1);
-		} else if (gamepad1.right_bumper) {
+		} else if (gamepad1.b) {
 			robot.tape.setPower(1);
 		} else {
 			robot.tape.setPower(0);
 		}
-		
+		//HOOK
 		if (gamepad1.x) {
 			robot.hooke.setPower(1); //hook up, robot down
 		} else if (gamepad1.y) {
@@ -38,22 +42,27 @@ public class TeleOpReal extends OpMode {
 			robot.hooke.setPower(0);
 		}
 		
+		//GAMEPAD 2
+		//ARM
 		robot.arm.setPower(gamepad2.left_stick_y);
-		if(gamepad2.dpad_up){
-			robot.rotation.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-			robot.rotation.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-		}
+		//ROTATION, WITH LIMITS.
 		if (gamepad2.x || !(gamepad2.right_stick_y > 0 && robot.rotation.getCurrentPosition() < -6680) //
 				&& !(gamepad2.right_stick_y < 0 && robot.rotation.getCurrentPosition() > 20)) {
 			robot.rotation.setPower(gamepad2.right_stick_y / 2);
 		} else {
 			robot.rotation.setPower(0);
 		}
-		
+		//reseting limits.
+		if (pastGamepad1X && !gamepad1.x) {
+			robot.rotation.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+			robot.rotation.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		}
+		pastGamepad1X = gamepad1.x;
+		//COLLECTION
 		if (gamepad2.left_bumper) {
-			robot.collection.setPower(1); //in
+			robot.collection.setPower(0.5); //in
 		} else if (gamepad2.right_bumper) {
-			robot.collection.setPower(-1); //out
+			robot.collection.setPower(-0.5); //out
 		} else {
 			robot.collection.setPower(0);
 		}
