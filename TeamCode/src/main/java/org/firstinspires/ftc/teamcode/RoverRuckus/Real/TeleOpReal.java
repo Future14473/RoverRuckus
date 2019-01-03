a3eea4e0b20b55bffd7065dc58a551f6167cf5ec
Benjamin Ye <BobBenBan@users.noreply.github.com>
2019-01-02 16:32:16 -0800

    Added YAAAAAAAAAAY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/Real/TeleOpReal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/Real/TeleOpReal.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.RoverRuckus.Real;
 
+import com.qualcomm.ftccommon.SoundPlayer;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -8,13 +9,19 @@ import org.firstinspires.ftc.teamcode.RoverRuckus.util.Robot;
 @TeleOp(name = "THE ACTUAL TELEOP", group = "teleop")
 public class TeleOpReal extends OpMode {
 	private Robot robot = new Robot();
+	private int yay = 0;
+	private boolean pastGamepad2a;
 	
 	@Override
 	public void init() {
 		robot.init(hardwareMap);
+		//FOR FUN, sounds
+		yay = hardwareMap.appContext.getResources().getIdentifier("yay", "raw",
+				hardwareMap.appContext.getPackageName());
+		if (yay != 0) SoundPlayer.getInstance().preload(hardwareMap.appContext, yay);
 	}
 	
-	private boolean pastGamepad1X = false;
+	private boolean pastGamepad1x = false;
 	
 	@Override
 	public void loop() {
@@ -23,13 +30,13 @@ public class TeleOpReal extends OpMode {
 		double speedMult = gamepad1.right_bumper ? 10 : (gamepad1.left_bumper ? 0.4 : 1);
 		double angle = Math.atan2(gamepad1.left_stick_x, -gamepad1.left_stick_y);
 		double speed = Math.pow(Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y), 1.7) * speedMult;
-		double turnRate = gamepad1.right_stick_x * 2; //prioritize turning over moving.
+		double turnRate = gamepad1.right_stick_x * speedMult * 2; //prioritize turning over moving.
 		robot.drive.moveAt(angle, speed, turnRate);
 		//TAPE
 		if (gamepad1.a) {
-			robot.tape.setPower(-1);
+			robot.tape.setPower(-1); //out
 		} else if (gamepad1.b) {
-			robot.tape.setPower(1);
+			robot.tape.setPower(1); //in
 		} else {
 			robot.tape.setPower(0);
 		}
@@ -49,15 +56,18 @@ public class TeleOpReal extends OpMode {
 		if (gamepad2.x || !(gamepad2.right_stick_y > 0 && robot.rotation.getCurrentPosition() < -6680) //
 				&& !(gamepad2.right_stick_y < 0 && robot.rotation.getCurrentPosition() > 20)) {
 			robot.rotation.setPower(gamepad2.right_stick_y / 2);
+			//negative = knob up, arm up
+			//positive = knob down, arm down
 		} else {
 			robot.rotation.setPower(0);
 		}
-		//reseting limits.
-		if (pastGamepad1X && !gamepad1.x) {
+		//resetting limits.
+		if (pastGamepad1x && !gamepad1.x) {
 			robot.rotation.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 			robot.rotation.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 		}
-		pastGamepad1X = gamepad1.x;
+		pastGamepad1x = gamepad1.x;
+		
 		//COLLECTION
 		if (gamepad2.left_bumper) {
 			robot.collection.setPower(0.5); //in
@@ -66,6 +76,8 @@ public class TeleOpReal extends OpMode {
 		} else {
 			robot.collection.setPower(0);
 		}
-		
+		if (gamepad2.a && !pastGamepad2a) {
+			SoundPlayer.getInstance().startPlaying(hardwareMap.appContext, yay);
+		}
 	}
 }
