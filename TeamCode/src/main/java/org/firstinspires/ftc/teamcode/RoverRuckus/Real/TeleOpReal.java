9238fac88805a7e1af2582f5937f8ccf15cccebb
Benjamin Ye <BobBenBan@users.noreply.github.com>
2019-01-05 21:51:18 -0800

    Teleop revisions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/Real/TeleOpReal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/Real/TeleOpReal.java
@@ -6,13 +6,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.RoverRuckus.util.Robot;
 
-@SuppressWarnings("SpellCheckingInspection")
 @TeleOp(name = "THE ACTUAL TELEOP", group = "teleop")
 public class TeleOpReal extends OpMode {
 	private Robot robot = new Robot();
 	private int yay = 0;
-	private boolean pastGamepad2a;
-	private boolean pastGamepad1x;
+	private boolean pastGamepad1y;
+	private boolean pastGamepad2a, pastGamepad2x;
+	private boolean reverseDrive = false;
 	
 	@Override
 	public void init() {
@@ -28,12 +28,20 @@ public class TeleOpReal extends OpMode {
 	@Override
 	public void loop() {
 		//GAMEPAD 1
+		//REVERSE
+		if (gamepad1.y && !pastGamepad1y) {
+			reverseDrive = !reverseDrive;
+		}
+		pastGamepad1y = gamepad1.y;
 		//MOVE BOT
-		double speedMult = gamepad1.right_bumper ? 10 : (gamepad1.left_bumper ? 0.4 : 1);
+		double speedMult = gamepad1.right_bumper ? 10 : (gamepad1.left_bumper ? 1.0 / 3 : 1);
 		double angle = Math.atan2(gamepad1.left_stick_x, -gamepad1.left_stick_y);
+		if (reverseDrive) angle += Math.PI;
 		double speed = Math.pow(Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y), 1.7) * speedMult;
 		double turnRate = gamepad1.right_stick_x * speedMult * 2; //prioritize turning over moving.
 		robot.drive.moveAt(angle, speed, turnRate);
+		telemetry.addData("DIRECTION", reverseDrive ? "HOOK FRONT" : "ARM FRONT");
+		telemetry.update();
 		//TAPE
 		if (gamepad1.a) {
 			robot.tape.setPower(-1); //out
@@ -55,31 +63,26 @@ public class TeleOpReal extends OpMode {
 		//ARM
 		robot.arm.setPower(gamepad2.left_stick_y);
 		//ROTATION, WITH LIMITS.
-		if (gamepad2.x || !(gamepad2.right_stick_y > 0 && robot.rotation.getCurrentPosition() < -6680) //
-				&& !(gamepad2.right_stick_y < 0 && robot.rotation.getCurrentPosition() > 20)) {
-			robot.rotation.setPower(gamepad2.right_stick_y / 2);
+		if (gamepad2.x || !(gamepad2.right_stick_y > 0 && robot.rotation.getCurrentPosition() < 0) //
+				&& !(gamepad2.right_stick_y < 0 && robot.rotation.getCurrentPosition() > 5480)) {
+			robot.rotation.setPower(gamepad2.right_stick_y / 1.5);
 			//negative = knob up, arm up
 			//positive = knob down, arm down
 		} else {
 			robot.rotation.setPower(0);
 		}
-		telemetry.addData("Gamepad1x:",gamepad1.x);
-		telemetry.addData("past Gamepad1x:", pastGamepad1x);
 		//resetting limits.
-		if (!gamepad1.x && pastGamepad1x) {
-			telemetry.addLine("WOOOOOOOT");
-			telemetry.update();
+		if (!gamepad2.x && pastGamepad2x) {
 			robot.rotation.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 			robot.rotation.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 		}
-		telemetry.update();
-		pastGamepad1x = gamepad1.x;
+		pastGamepad2x = gamepad2.x;
 		
 		//COLLECTION
 		if (gamepad2.left_bumper) {
-			robot.collection.setPower(0.5); //in
+			robot.collection.setPower(0.8); //in
 		} else if (gamepad2.right_bumper) {
-			robot.collection.setPower(-0.5); //out
+			robot.collection.setPower(-0.8); //out
 		} else {
 			robot.collection.setPower(0);
 		}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/testing/Measure.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/testing/Measure.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.RoverRuckus.testing;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,24 +8,22 @@ import org.firstinspires.ftc.teamcode.RoverRuckus.util.Robot;
 @TeleOp(name = "Measurement", group = "measure")
 public class Measure extends OpMode {
 	private Robot robot = new Robot();
+	private DcMotor motor;
 	
 	@Override
 	public void init() {
 		robot.init(hardwareMap);
+		motor = robot.arm;
 	}
 	
 	@Override
 	public void loop() {
 		if (gamepad1.a) {
-			robot.hooke.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-			robot.hooke.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+			motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+			motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 		}
-		if (gamepad1.x) {
-			robot.rotation.setPower(0.3);
-		} else if (gamepad1.y) {
-			robot.rotation.setPower(-0.3);
-		} else robot.rotation.setPower(0);
-		telemetry.addData("Rot pos:", robot.rotation.getCurrentPosition());
+		motor.setPower(gamepad1.right_stick_x);
+		telemetry.addData("Pos:", motor.getCurrentPosition());
 		telemetry.update();
 	}
 }
